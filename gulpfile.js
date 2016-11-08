f3796a172ce87889c2d31e8bc18e01ed4f2fa3ec
Lionel Grondin <lionelgrondin2@gmail.com>
2016-11-08 12:00:22 +0100

    Gulpfile plumber

--- gulpfile.js
+++ gulpfile.js
@@ -3,15 +3,15 @@ var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 var autoprefixer = require('gulp-autoprefixer');
 var plumber = require('gulp-plumber');
-var coffee = require('gulp-coffee');
 
-gulp.src('./src/*.ext')
-    .pipe(plumber())
-    .pipe(coffee())
-    .pipe(gulp.dest('./dist'));
-		
 gulp.task('css', function(){
 	gulp.src('css/global.scss')
+    .pipe(plumber({
+        handleError: function (err) {
+            console.log(err);
+            this.emit('end');
+        }
+    }))
 	.pipe(sourcemaps.init())
 	.pipe(sass())
 	.pipe(sourcemaps.write())
