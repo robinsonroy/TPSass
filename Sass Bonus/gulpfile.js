4e66578556eedf1b0c2050e1ecd22b19e951dbb2
robinson Roy <robinson.roy@me.com>
2016-11-09 14:25:47 +0100

    add Sass file extension bonus

---
+++ Sass Bonus/gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
+var plumber = require('gulp-plumber');
+
+gulp.task('css', function(){
+	gulp.src('css/global.sass')
+    .pipe(plumber({
+        handleError: function (err) {
+            console.log(err);
+            this.emit('end');
+        }
+    }))
+	.pipe(sourcemaps.init())
+	.pipe(sass())
+	.pipe(sourcemaps.write())
+	.pipe(autoprefixer({
+		 browsers: ['last 10 versions']
+	}))
+	.pipe(gulp.dest('css/'))
+});
+
+gulp.task('watch', function(){
+	gulp.watch('css/*.sass', ['css']);
+});
+
+gulp.task('default', ['css', 'watch']);
